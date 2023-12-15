219cd28971ef26bb39d356c2a36dbc4af5f26ccb
shivangi-jain-08 <shivangi.jain@kalvium.community>
2023-12-15 09:33:49 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
+import React from 'react';
 import './App.css';
-import elephant from "./images/elephant.jpeg";
+import elephant from './images/elephant.jpeg';
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
-function App() {
-  // code here
-
-  let imageArr = imageData();
-
-  return(
-    <div>
-      <h1>Kalvium Gallary</h1>
-      <div className='parent'>
-        <img src={imageArr[0].img} className='image' alt="" />
-        <img src={imageArr[1].img} alt="" className='image' />
-        <img src={imageArr[2].img} alt="" className='image' />
-        <img src={imageArr[3].img} alt="" className='image' />
-      </div>
+const ImageGallery = ({ imageData }) => {
+  return (
+    <div className="parent">
+      {imageData.map((item) => (
+        <img key={item.id} src={item.img} alt="" className="image" />
+      ))}
     </div>
-  )
-}
+  );
+};
+
+const App = () => {
+  const imageData = [
+    { id: 1, img: elephant },
+    { id: 2, img: elephant },
+    { id: 3, img: elephant },
+    { id: 4, img: elephant },
+  ];
+
+  return (
+    <div>
+      <h1>Kalvium Gallery</h1>
+      <ImageGallery imageData={imageData} />
+    </div>
+  );
+};
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,43 +1,38 @@
-import { Component } from "react";
-import "./App.css";
-import elephant from "./images/elephant.jpeg";
+import React, { Component } from 'react';
+import './App.css';
+import elephant from './images/elephant.jpeg';
+
+class ImageGallery extends Component {
+  render() {
+    const { imageData } = this.props;
+
+    return (
+      <div className="parent">
+        {imageData.map((item) => (
+          <img key={item.id} src={item.img} alt="" className="image" />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default class AppClass extends Component {
   imageData = () => {
-    let data = [
-      {
-        id: 1,
-        img: elephant,
-      },
-      {
-        id: 2,
-        img: elephant,
-      },
-      {
-        id: 3,
-        img: elephant,
-      },
-      {
-        id: 4,
-        img: elephant,
-      },
+    return [
+      { id: 1, img: elephant },
+      { id: 2, img: elephant },
+      { id: 3, img: elephant },
+      { id: 4, img: elephant },
     ];
-    return data;
   };
 
-  // code here
-
   render() {
-    let imageArr = this.imageData()
+    const imageArr = this.imageData();
+
     return (
       <div>
-        <h1>Kalvium Gallary</h1>
-        <div className="parent">
-          <img src={imageArr[0].img} className="image" alt="" />
-          <img src={imageArr[1].img} alt="" className="image" />
-          <img src={imageArr[2].img} alt="" className="image" />
-          <img src={imageArr[3].img} alt="" className="image" />
-        </div>
+        <h1>Kalvium Gallery</h1>
+        <ImageGallery imageData={imageArr} />
       </div>
     );
   }
